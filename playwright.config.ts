6134cee6d01406cc9a6bb49b4509427cac60bc05
Anton Schmidt <schmidtant@hotmail.de>
2022-09-27 14:34:23 +0200

    test: configre playwright to run cross browser tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -46,35 +46,24 @@ const config: PlaywrightTestConfig = {
         ...devices['Desktop Chrome'],
       },
     },
-    // {
-    //   name: 'Desktop Firefox',
-    //   use: {
-    //     ...devices['Desktop Firefox'],
-    //   },
-    // },
-    // {
-    //   name: 'Desktop Safari',
-    //   use: {
-    //     ...devices['Desktop Safari'],
-    //   },
-    // },
-    // {
-    //   name: 'Desktop Edge',
-    //   use: {
-    //     ...devices['Desktop Edge'],
-    //   },
-    // },
-    // Test against mobile viewports.
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: {
-    //     ...devices['Pixel 5'],
-    //   },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: devices['iPhone 12'],
-    // },
+    {
+      name: 'Desktop Firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+      },
+    },
+    {
+      name: 'Desktop Safari',
+      use: {
+        ...devices['Desktop Safari'],
+      },
+    },
+    {
+      name: 'Desktop Edge',
+      use: {
+        ...devices['Desktop Edge'],
+      },
+    },
   ],
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
   outputDir: 'test-results/',
